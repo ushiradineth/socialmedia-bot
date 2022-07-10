417910b43723932589e16a9fe04c71dcff3df772
ushiradineth <ushiradineth@gmail.com>
2022-07-10 23:46:40 +0530

    90% done with instagram (other than stories)

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import strftime
+from instascrape import Reel
 import time
 import tweepy
 import credentials
@@ -35,10 +36,10 @@ def tiktok():
             data = json.load(f)
             latest_tiktok = (data["tiktokLink"])
 
-        driver.get("https://www.tiktok.com/@le_sserafim")
+        driver.get("https://www.tiktok.com/@aespa_official")
 
         #waits until the page loads
-        div = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "tiktok-yvmafn-DivVideoFeedV2")))
+        div = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[2]/div[2]/div/div[2]/div[2]/div")))
 
         #how many new tiktoks were made since the last check
         len = 0
@@ -52,21 +53,20 @@ def tiktok():
 
         #tweeting the new tiktoks
         for i in range(len, 0, -1):
-            div = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "tiktok-yvmafn-DivVideoFeedV2")))
+            div = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[2]/div[2]/div/div[2]/div[2]/div")))
             link = div.find_element(By.XPATH, "/html/body/div[2]/div[2]/div[2]/div/div[2]/div[2]/div/div[" + str(i) + "]/div[1]/div/div/a").get_attribute("href")
             bio = div.find_element(By.XPATH, "/html/body/div[2]/div[2]/div[2]/div/div[2]/div[2]/div/div[" + str(i) + "]/div[2]/a").get_attribute("title")
-            biotext = bio.split("#")
-
+            
             #fixing hashtags
+            biotext = bio.split("#")
             if re.findall('with', bio):
                 biotext[0] = biotext[0] + "#" + biotext[1]
                 
-            if re.findall('from', bio):
-                if re.findall("@", bio):
-                    biotemp = biotext[0].split("@")
-                    biotext[0] = biotemp[0] + "@/" + biotemp[1]
+            if re.findall("@", bio):
+                biotemp = biotext[0].split("@")
+                biotext[0] = biotemp[0] + "@/" + biotemp[1]
 
-            hashtaglist = "#CHAEWON", "#SAKURA", "#YUNJIN", "#KAZUHA", "#EUNCHAE", "#채원", "#사쿠라", "#윤진", "#카즈하", "#은채"
+            hashtaglist = "#KARINA", "#카리나", "#GISELLE", "#지젤", "#WINTER", "#윈터", "#NINGNING", "#닝닝"
             hashtags = ""
             for hashtag in hashtaglist:
                 if re.findall(hashtag, bio):
@@ -74,12 +74,12 @@ def tiktok():
             
             #downloading the video
             driver.get(div.find_element(By.XPATH, "/html/body/div[2]/div[2]/div[2]/div/div[2]/div[2]/div/div[" + str(i) + "]/div[1]/div/div/a").get_attribute("href"))
-            div1 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "tiktok-13egybz-DivBasicPlayerWrapper")))
-            videolink = div1.find_element(By.XPATH, "/html/body/div[2]/div[2]/div[2]/div[1]/div[1]/div/div[2]/div[1]/div/div[1]/div/div/video").get_attribute("src")
+            div1 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[2]/div[2]/div[1]/div[3]/div/div[1]/div[1]/div[2]/div/div/div")))
+            videolink = div1.find_element(By.XPATH, "/html/body/div[2]/div[2]/div[2]/div[1]/div[3]/div/div[1]/div[1]/div[2]/div/div/div/video").get_attribute("src")
             urllib.request.urlretrieve(videolink, 'tiktok.mp4')
 
             #tweeting
-            tweet = "[TikTok Update - " + str(strftime("%d%m%y")) + "] (" + link + ")" + "\n\n" + " ".join(biotext[0].strip().split()) + "\n\n#LE_SSERAFIM #르세라핌 #IMFEARLESS" + hashtags
+            tweet = "[TikTok Update - " + str(strftime("%d%m%y")) + "] (" + link + ")" + "\n\n" + " ".join(biotext[0].strip().split()) + "\n\n#aespa #에스파" + hashtags
             upload_result = api.media_upload('tiktok.mp4')
             api.update_status(status=tweet, media_ids=[upload_result.media_id_string])
             os.remove('tiktok.mp4')
@@ -87,11 +87,14 @@ def tiktok():
             
             #posting the new link to the json file for the next run
             if i == 1:
-                with open('data.json', 'w') as outfile:
-                    data = { "tiktokLink": link }
-                    json.dump(data, outfile)
+                with open('data.json', 'r+') as outfile:
+                    data = json.load(outfile)
+                    data['tiktokLink'] = link
+                    outfile.seek(0)
+                    json.dump(data, outfile, indent=4)
+                    outfile.truncate()
             
-            driver.get("https://www.tiktok.com/@le_sserafim")
+            driver.get("https://www.tiktok.com/@aespa_official")
     
     except Exception as e:
         errorDM("tiktok", e)
@@ -111,41 +114,39 @@ def instagram():
         logindone = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/section/main/div/div/div/section/div/button")))
 
         i=1
-        profilelinks = "https://www.instagram.com/_chaechae_1/", "https://www.instagram.com/39saku_chan/", "https://www.instagram.com/k_a_z_u_h_a__/"
+        profilelink = "https://www.instagram.com/aespa_official"
 
-        for profilelink in profilelinks:
-            #getting the last tweeted instagram link from the json file
-            with open('data.json', 'r') as f:
-                data = json.load(f)
-                latest_link = (data["instagramLink" + str(i)])
-                i=i+1
+        #getting the last tweeted instagram link from the json file
+        with open('data.json', 'r') as f:
+            data = json.load(f)
+            latest_link = (data["instagramLink"])
+            i=i+1
 
-            #go to the instagram page
-            driver.get(profilelink)
-            #waits until the page loads
-            instagramPage = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "_aa-i")))
-            postRows = instagramPage.find_elements(By.CLASS_NAME, "_ac7v")
-            newPosts=""
+        #go to the instagram page
+        driver.get(profilelink)
+        #waits until the page loads
+        instagramPage = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "_aa-i")))
+        postRows = instagramPage.find_elements(By.CLASS_NAME, "_ac7v")
+        newPosts=[]
 
-            #getting the new posts
-            for postRow in postRows:
-                posts = postRow.find_elements(By.CLASS_NAME, "_aabd")
-                for post in posts:
-                    link = post.find_element(By.TAG_NAME, "a").get_attribute("href")
-                    if (link==latest_link):
-                        break
-                    print("link: ", link)
-                    newPosts = newPosts + link
-                else:
-                    continue
-                break
+        c=False
 
-            print("newPosts: ", newPosts)
-            newPosts = tuple(newPosts)
-            print("newPosts: ", newPosts)
+        #getting the new posts
+        for postRow in postRows:
+            posts = postRow.find_elements(By.CLASS_NAME, "_aabd")
+            for post in posts:
+                link = post.find_element(By.TAG_NAME, "a").get_attribute("href")
+                if (link==latest_link):
+                    break
+                newPosts.append(link)
+                c=True
+            else:
+                continue
+            break
+
+        if c == True:
             #tweeting the posts
             for newPost in newPosts:
-                print("newPost: ", newPost)
                 driver.get(newPost)
                 caption=""
                 len=0
@@ -163,83 +164,145 @@ def instagram():
                         caption=""     
 
                 except:
-                    multipost = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "_acay")))  
-                    posts = ""
-                    links = ""
-
                     try:
-                        for i in range(1, 10):
-                            posts = driver.find_elements(By.CLASS_NAME, "_acaz")
-                            driver.find_element(By.CLASS_NAME, "_aahi").click()
+                        multipost = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "_acay")))  
+                        posts = ""
+                        links = ""
+
+                        try:
+                            for i in range(1, 10):
+                                posts = driver.find_elements(By.CLASS_NAME, "_acaz")
+                                driver.find_element(By.CLASS_NAME, "_aahi").click()
+                        except:
+                            pass
+
+                        for post in posts:
+                            try:
+                                link = post.find_element(By.TAG_NAME, "img").get_attribute("src")
+                                urllib.request.urlretrieve(link, str(i)+ ".png")
+                                len=len+1
+                            except:
+                                SESSIONID = driver.get_cookie("sessionid")
+                                SESSIONID = SESSIONID["value"]
+
+                                headers = {
+                                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+                                    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.74 \
+                                    Safari/537.36 Edg/79.0.309.43",
+                                    "cookie": f'sessionid={SESSIONID};'
+                                }
+                                insta_reel = Reel(newPost)
+
+                                insta_reel.scrape(headers=headers)
+                                insta_reel.download(str(i)+ ".mp4")
+
+                                len=len+1
+                                                                    
+                        try:
+                            caption = multipost.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div/div[1]/div/div/div[1]/div[1]/section/main/div[1]/div[1]/article/div/div[2]/div/div[2]/div[1]/ul/div/li/div/div/div[2]/div[1]/span").text
+                            if re.findall("@", caption):
+                                biotemp = caption.split("@")
+                                caption = biotemp[0] + "@/" + biotemp[1]
+                        except:
+                            caption=""  
+
                     except:
-                        pass
+                        try:
+                            video = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "_ab1c")))
+                            link = video.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div/div[1]/div/div/div/div[1]/div[1]/section/main/div[1]/div[1]/article/div/div[1]/div/div/div/div/div/video").get_attribute("src")
+                            urllib.request.urlretrieve(link, 'instagram.mp4')
 
-                    for post in posts:
-                        link = post.find_element(By.TAG_NAME, "img").get_attribute("src")
-                        links = links + link
+                        except:
+                            video = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "_ab1c")))
+                            link = video.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div/div[1]/div/div/div/div[1]/div[1]/section/main/div[1]/div[1]/article/div/div[1]/div/div/div/div/div/video").get_attribute("src")
+                            urllib.request.urlretrieve(link, 'instagram.mp4')
 
-                    for link in links:
-                        urllib.request.urlretrieve(link, str(i)+ ".png")
-                        len=len+1
-                    
-                    try:
-                        caption = multipost.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div/div[1]/div/div/div[1]/div[1]/section/main/div[1]/div[1]/article/div/div[2]/div/div[2]/div[1]/ul/div/li/div/div/div[2]/div[1]/span").text
-                        if re.findall("@", caption):
-                            biotemp = caption.split("@")
-                            caption = biotemp[0] + "@/" + biotemp[1]
-                    except:
-                        caption=""   
-                
-                username = multipost.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div/div[1]/div/div/div[1]/div[1]/section/main/div[1]/div[1]/article/div/div[2]/div/div[1]/div/header/div[2]/div[1]/div[1]/div[1]/span/a").text
-                tweet = "[Instagram Update - " + str(strftime("%d%m%y")) + " " + username + "] (" + link + ")" + "\n\n" + " " + caption + "\n\n#LE_SSERAFIM #르세라핌 #IMFEARLESS"
+                tweet = "[Instagram Update - " + str(strftime("%d%m%y")) + "] (" + link + ")" + "\n\n" + " " + caption + "\n\n#aespa #에스파"
 
-                files = ""
+                files = []
 
-                if len == 1:
-                    api.update_status_with_media(status=tweet, filename="1.png")
-                elif len <= 4:
+                if len <= 4:
                     for i in range(1, len+1):
-                        files = files + (str(i) + ".png")
-                        api.update_status_with_media(status=tweet, filename=files)
+                        try:
+                            upload_result = api.media_upload(str(i) + ".png")
+                        except:
+                            upload_result = api.media_upload(str(i) + ".mp4")
+                        files.append(upload_result.media_id_string)
+
+                    api.update_status(status=tweet, media_ids=files)
+                    upload_result=""
+
                 elif len <= 8:
                     for i in range(1, 5):
-                        files = files + (str(i) + ".png")
-                        api.update_status_with_media(status=tweet, filename=files)
+                        try:
+                            upload_result = api.media_upload(str(i) + ".png")
+                        except:
+                            upload_result = api.media_upload(str(i) + ".mp4")
+                        files.append(upload_result.media_id_string)
+
+                    tweet1 = api.update_status(status=tweet, media_ids=files)
+                    upload_result=""
+                    files = []
+
                     for i in range(5, len+1):
-                        files = files + (str(i) + ".png")
-                        status_list = api.user_timeline(user_id=credentials.RECIPIENT_ID)
-                        status = status_list[0]
-                        json_str = status._json
-                        tweetid = json_str["id"]
-                        api.update_status_with_media(status=tweet, filename=files, in_reply_to_status_id=tweetid)
+                        try:
+                            upload_result = api.media_upload(str(i) + ".png")
+                        except:
+                            upload_result = api.media_upload(str(i) + ".mp4")
+                        files.append(upload_result.media_id_string)
+
+                    api.update_status(status=tweet, media_ids=files, in_reply_to_status_id=tweet1.id)
+                    upload_result=""
+
                 else:
                     for i in range(1, 5):
-                        files = files + (str(i) + ".png")
-                        api.update_status_with_media(status=tweet, filename=files)
+                        try:
+                            upload_result = api.media_upload(str(i) + ".png")
+                        except:
+                            upload_result = api.media_upload(str(i) + ".mp4")
+                        files.append(upload_result.media_id_string)
+
+                    tweet1 = api.update_status(status=tweet, media_ids=files)
+                    upload_result=""
+                    files = []
+
                     for i in range(5, 9):
-                        files = files + (str(i) + ".png")
-                        status_list = api.user_timeline(user_id=credentials.RECIPIENT_ID)
-                        status = status_list[0]
-                        json_str = status._json
-                        tweetid = json_str["id"]
-                        api.update_status_with_media(status=tweet, filename=files, in_reply_to_status_id=tweetid)
+                        try:
+                            upload_result = api.media_upload(str(i) + ".png")
+                        except:
+                            upload_result = api.media_upload(str(i) + ".mp4")
+                        files.append(upload_result.media_id_string)
+
+                    tweet2 = api.update_status(status=tweet, media_ids=files, in_reply_to_status_id=tweet1.id)
+                    upload_result=""
+                    files = []
+
                     for i in range(9, len+1):
-                        files = files + (str(i) + ".png")
-                        status_list = api.user_timeline(user_id=credentials.RECIPIENT_ID)
-                        status = status_list[0]
-                        json_str = status._json
-                        tweetid = json_str["id"]
-                        api.update_status_with_media(status=tweet, filename=files, in_reply_to_status_id=tweetid)
+                        try:
+                            upload_result = api.media_upload(str(i) + ".png")
+                        except:
+                            upload_result = api.media_upload(str(i) + ".mp4")
+                        files.append(upload_result.media_id_string)
+
+                    api.update_status(status=tweet, media_ids=files, in_reply_to_status_id=tweet2.id)
+                    upload_result=""
+                    files = []
 
                 for i in range(1, len+1):
-                    os.remove(str(i) + ".png")
+                    try:
+                        os.remove(str(i) + ".png")
+                    except:
+                        os.remove(str(i) + ".mp4")
                 
                 #posting the new link to the json file for the next run
-                with open('data.json', 'w') as outfile:
-                    data = { ["instagramLink" + str(i)]: newPosts[0] }
-                    json.dump(data, outfile)    
+                with open('data.json', 'r+') as outfile:
+                    data = json.load(outfile)
+                    data['instagramLink'] = link
+                    outfile.seek(0)
+                    json.dump(data, outfile, indent=4)
+                    outfile.truncate() 
 
-# tiktok()
+#tiktok()
 instagram()
 
 # if int(strftime("%M"))%10 == 0:
